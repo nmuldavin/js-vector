6f94c8f53ffc26384775aaaafdbcaedd0bcea01e
nmuldavin <nmuldavin@gmail.com>
2016-07-23 10:13:56 -0700

    Writing basic methods and tests

--- src/jsvector.js
+++ src/jsvector.js
@@ -1,13 +1,15 @@
 
 var Vector = function(length) {
 
-  var inst;
+  var inst, i;
 
   if (!length) {
     inst = [];
   } else {
     inst = new Array(length);
-    inst.fill(0);
+    for(i = 0; i < length; i++) {
+      inst[i] = 0;
+    }
   }
 
   inst.__proto__ = Vector.prototype;
@@ -20,27 +22,99 @@ Vector.prototype = Object.create(Array.prototype);
 addVectorMethods(Vector.prototype);
 
 Vector.from = function() {
-  var inst = new Vector();
-  return inst.push(Array.from(arguments));
+  var inst = Array.from.apply(this, arguments);
+  inst.__proto__ = Vector.prototype;
+  return inst;
 };
 
 Vector.of = function() {
-  var inst = new Vector();
-  return inst.push(Array.from(arguments));
+  var inst = Array.of.apply(this, arguments);
+  inst.__proto__ = Vector.prototype;
+  return inst;
 };
 
 function addVectorMethods(that) {
 
-  that.testMethod = function() {
-    return new Vector();
+  that.elementWise = function(operation, ...vecs) {
+    var i, out = new Vector();
+    var numVecs = vecs.length;
+    var length = vecs[0].length;
+    var getIthValues = function(index, args) {
+      var j, values = new Vector();
+
+      for(j = 0; j < numVecs; j++) {
+        values[j] = args[j][index];
+      }
+
+      return values;
+    };
+
+
+    for(i = 0; i < length; i++) {
+      out[i] = operation(...getIthValues(i, vecs));
+    }
+
+    return out;
   };
 
-  that.print = function() {
-    console.log("It worked");
+  that.isEqual = function(other) {
+    var i, length=this.length;
+
+    for(i = 0; i < length; i++) {
+      if (this[i] !== other[i]) {
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  that.elIsEqual = function(other) {
+    var out = new Vector();
+    var i, length=this.length;
+
+    for(i = 0; i < length; i++) {
+      out[i] = this[i] === other[i];
+    }
+
+    return out;
+  };
+
+  that.plus = function(other) {
+    var out = new Vector();
+    var i, length = this.length;
+
+    for(i = 0; i < length; i++) {
+      out[i] = this[i] + other[i];
+    }
+
+    return out;
+  };
+
+  that.pluseq = function(other) {
+    var i, length = this.length;
+
+    for(i = 0; i < length; i++) {
+      this[i] += other[i];
+    }
+
+  };
+
+  that.minus = function(other) {
+    var out = new Vector();
+    var i, length = this.length;
+
+    for(i = 0; i < length; i++) {
+      out[i] = this[i] - other[i];
+    }
+
+    return out;
   };
 
   return that;
 }
+
+
 
 module.exports = {
   Vector: Vector
--- test/jsvector.js
+++ test/jsvector.js
@@ -2,15 +2,23 @@ var should = require('chai').should();
 var Vector = require('../dist/jsvector.js').Vector;
 
 describe('jsvector', function() {
-  describe('Vector()', function() {
-    var v;
+
+  var v;
+
+  describe('Vector() Constructor', function() {
 
     beforeEach(function() {
       v = new Vector();
     });
 
-    it('Should return an empty array', function() {
+    it('Vector() should return an empty array', function() {
       v.length.should.equal(0);
+    });
+
+    it('Vector(val) Should return a Vector of specified length initialized to zero', function() {
+      v = new Vector(5);
+      v.length.should.eql(5);
+      v.forEach(val => val.should.eql(0));
     });
 
     it('Should return a new object', function() {
@@ -18,6 +26,49 @@ describe('jsvector', function() {
     });
 
     it('Should return an Array', function() {
+      v.should.be.an.instanceof(Vector);
+    });
+
+    it('Should have of() method', function() {
+      var nv = Vector.of(1, 2, 3);
+      nv.should.be.an.instanceof(Vector);
+      nv.length.should.equal(3);
+    });
+
+    it('Should have from() method', function() {
+      v = Vector.from([1, 2, 3], x => x + x);
+      v.should.be.an.instanceof(Vector);
+      v.length.should.equal(3);
+      v[0].should.eql(2);
+    });
+  });
+
+  describe('methods', function() {
+    beforeEach(function() {
+      v = Vector.of(1, 2, 3);
+      w = Vector.of(2, 3, 4);
+    });
+
+    it('should have a method to check Vector equality', function() {
+      w = v.isEqual([1, 2, 3]);
+      w.should.be.true;
+      w = v.isEqual([2, 3, 4]);
+      w.should.be.false;
+    });
+
+    it('should have an elementwise equality check', function() {
+      v.elIsEqual([1, 4, 3]).isEqual([true, false, true]).should.be.true;
+    });
+
+    it('should add two vectors', function() {
+      v = v.plus(w);
+      v.isEqual([3, 5, 7]).should.be.true;
+      v.should.be.an.instanceof(Vector);
+    });
+
+    it('should subtract two vectors', function() {
+      v = v.minus(w);
+      v.isEqual([-1, -1, -1]).should.be.true;
       v.should.be.an.instanceof(Vector);
     });
   });
