ac7a532cfd8431f1f2b2800b2409038778189a11
nmuldavin <nmuldavin@gmail.com>
2016-07-25 21:35:50 -0700

    Finished Methods

--- src/jsvector.js
+++ src/jsvector.js
@@ -1,66 +1,68 @@
 
-var Vector = function(length) {
-
-  var inst, i;
+function Vector(length) {
+  let inst;
+  let i;
 
   if (!length) {
     inst = [];
   } else {
     inst = new Array(length);
-    for(i = 0; i < length; i++) {
+
+    for (i = 0; i < length; i++) {
       inst[i] = 0;
     }
   }
 
   inst.__proto__ = Vector.prototype;
-
   return inst;
-};
+}
 
 Vector.prototype = Object.create(Array.prototype);
 
-addVectorMethods(Vector.prototype);
+Vector.from = function from(...args) {
+  const inst = Array.from(...args);
 
-Vector.from = function() {
-  var inst = Array.from.apply(this, arguments);
   inst.__proto__ = Vector.prototype;
   return inst;
 };
 
-Vector.of = function() {
-  var inst = Array.of.apply(this, arguments);
+Vector.of = function of(...args) {
+  const inst = Array.of(...args);
+
   inst.__proto__ = Vector.prototype;
   return inst;
 };
 
 function addVectorMethods(that) {
+  that.elementWise = function elementWise(operation, ...vecs) {
+    const out = new Vector();
+    const numVecs = vecs.length;
+    const length = vecs[0].length;
+    let i;
 
-  that.elementWise = function(operation, ...vecs) {
-    var i, out = new Vector();
-    var numVecs = vecs.length;
-    var length = vecs[0].length;
-    var getIthValues = function(index, args) {
-      var j, values = new Vector();
+    function getIthValues(index, args) {
+      const values = new Vector();
+      let j;
 
-      for(j = 0; j < numVecs; j++) {
+      for (j = 0; j < numVecs; j++) {
         values[j] = args[j][index];
       }
 
       return values;
-    };
+    }
 
-
-    for(i = 0; i < length; i++) {
+    for (i = 0; i < length; i++) {
       out[i] = operation(...getIthValues(i, vecs));
     }
 
     return out;
   };
 
-  that.isEqual = function(other) {
-    var i, length=this.length;
+  that.isEqual = function isEqual(other) {
+    const length = this.length;
+    let i;
 
-    for(i = 0; i < length; i++) {
+    for (i = 0; i < length; i++) {
       if (this[i] !== other[i]) {
         return false;
       }
@@ -69,53 +71,189 @@ function addVectorMethods(that) {
     return true;
   };
 
-  that.elIsEqual = function(other) {
-    var out = new Vector();
-    var i, length=this.length;
+  that.elIsEqual = function elIsEqual(other) {
+    const out = new Vector();
+    const length = this.length;
+    let i;
 
-    for(i = 0; i < length; i++) {
-      out[i] = this[i] === other[i];
+    for (i = 0; i < length; i++) {
+      out[i] = 0 + (this[i] === other[i]);
     }
 
     return out;
   };
 
-  that.plus = function(other) {
-    var out = new Vector();
-    var i, length = this.length;
+  that.plus = function plus(other) {
+    const out = new Vector();
+    const length = this.length;
+    let i;
 
-    for(i = 0; i < length; i++) {
+    for (i = 0; i < length; i++) {
       out[i] = this[i] + other[i];
     }
 
     return out;
   };
 
-  that.pluseq = function(other) {
-    var i, length = this.length;
+  that.plusEq = function plusEq(other) {
+    const length = this.length;
+    let i;
 
-    for(i = 0; i < length; i++) {
+    for (i = 0; i < length; i++) {
       this[i] += other[i];
     }
-
   };
 
-  that.minus = function(other) {
-    var out = new Vector();
-    var i, length = this.length;
+  that.minus = function minus(other) {
+    const out = new Vector();
+    const length = this.length;
+    let i;
 
-    for(i = 0; i < length; i++) {
+    for (i = 0; i < length; i++) {
       out[i] = this[i] - other[i];
     }
 
     return out;
   };
 
+  that.minusEq = function minusEq(other) {
+    const length = this.length;
+    let i;
+
+    for (i = 0; i < length; i++) {
+      this[i] -= other[i];
+    }
+  };
+
+  that.times = function times(scalar) {
+    const out = new Vector();
+    const length = this.length;
+    let i;
+
+    for (i = 0; i < length; i++) {
+      out[i] = this[i] * scalar;
+    }
+
+    return out;
+  };
+
+  that.timesEq = function timesEq(scalar) {
+    const length = this.length;
+    let i;
+
+    for (i = 0; i < length; i++) {
+      this[i] *= scalar;
+    }
+  };
+
+  that.dividedBy = function dividedBy(scalar) {
+    const out = new Vector();
+    const length = this.length;
+    let i;
+
+    for (i = 0; i < length; i++) {
+      out[i] = this[i] / scalar;
+    }
+
+    return out;
+  };
+
+  that.dividedByEq = function dividedByEq(scalar) {
+    const length = this.length;
+    let i;
+
+    for (i = 0; i < length; i++) {
+      this[i] /= scalar;
+    }
+  };
+
+  that.ewTimes = function ewTimes(other) {
+    const out = new Vector();
+    const length = this.length;
+    let i;
+
+    for (i = 0; i < length; i++) {
+      out[i] = this[i] * other[i];
+    }
+
+    return out;
+  };
+
+  that.ewTimesEq = function ewTimesEq(other) {
+    const length = this.length;
+    let i;
+
+    for (i = 0; i < length; i++) {
+      this[i] *= other[i];
+    }
+  };
+
+  that.ewDividedBy = function ewDividedBy(other) {
+    const out = new Vector();
+    const length = this.length;
+    let i;
+
+    for (i = 0; i < length; i++) {
+      out[i] = this[i] / other[i];
+    }
+
+    return out;
+  };
+
+  that.ewDividedByEq = function ewTimesEq(other) {
+    const length = this.length;
+    let i;
+
+    for (i = 0; i < length; i++) {
+      this[i] /= other[i];
+    }
+  };
+
+  that.toThe = function toThe(power) {
+    const out = new Vector();
+    const length = this.length;
+    let i;
+
+    for (i = 0; i < length; i++) {
+      out[i] = Math.pow(this[i], power);
+    }
+
+    return out;
+  };
+
+  that.ewToThe = function ewToThe(other) {
+    const out = new Vector();
+    const length = this.length;
+    let i;
+
+    for (i = 0; i < length; i++) {
+      out[i] = Math.pow(this[i], other[i]);
+    }
+
+    return out;
+  };
+
+  that.dot = function dot(other) {
+    const length = this.length;
+    let out = 0;
+    let i;
+
+    for (i = 0; i < length; i++) {
+      out += this[i] * other[i];
+    }
+
+    return out;
+  };
+
+  that.norm = function norm() {
+    return Math.sqrt(this.dot(this));
+  };
+
   return that;
 }
 
-
+addVectorMethods(Vector.prototype);
 
 module.exports = {
-  Vector: Vector
+  Vector,
 };
--- test/jsvector.js
+++ test/jsvector.js
@@ -1,41 +1,39 @@
-var should = require('chai').should();
-var Vector = require('../dist/jsvector.js').Vector;
+require('chai').should();
+const Vector = require('../dist/jsvector.js').Vector;
 
-describe('jsvector', function() {
+describe('jsvector', () => {
+  let v;
 
-  var v;
-
-  describe('Vector() Constructor', function() {
-
-    beforeEach(function() {
+  describe('Vector() Constructor', () => {
+    beforeEach(() => {
       v = new Vector();
     });
 
-    it('Vector() should return an empty array', function() {
+    it('Vector() should return an empty array', () => {
       v.length.should.equal(0);
     });
 
-    it('Vector(val) Should return a Vector of specified length initialized to zero', function() {
+    it('Vector(val) Should return a Vector of specified length initialized to zero', () => {
       v = new Vector(5);
       v.length.should.eql(5);
       v.forEach(val => val.should.eql(0));
     });
 
-    it('Should return a new object', function() {
+    it('Should return a new object', () => {
       v.should.be.an.instanceof(Vector);
     });
 
-    it('Should return an Array', function() {
+    it('Should return an Array', () => {
       v.should.be.an.instanceof(Vector);
     });
 
-    it('Should have of() method', function() {
-      var nv = Vector.of(1, 2, 3);
+    it('Should have of() method', () => {
+      const nv = Vector.of(1, 2, 3);
       nv.should.be.an.instanceof(Vector);
       nv.length.should.equal(3);
     });
 
-    it('Should have from() method', function() {
+    it('Should have from() method', () => {
       v = Vector.from([1, 2, 3], x => x + x);
       v.should.be.an.instanceof(Vector);
       v.length.should.equal(3);
@@ -43,33 +41,121 @@ describe('jsvector', function() {
     });
   });
 
-  describe('methods', function() {
-    beforeEach(function() {
+  describe('methods', () => {
+    let w;
+    let x;
+    let y;
+
+    beforeEach(() => {
       v = Vector.of(1, 2, 3);
       w = Vector.of(2, 3, 4);
+      x = null;
+      y = Vector.of(5, 10, 15);
     });
 
-    it('should have a method to check Vector equality', function() {
+    it('should have a method to check Vector equality', () => {
       w = v.isEqual([1, 2, 3]);
       w.should.be.true;
       w = v.isEqual([2, 3, 4]);
       w.should.be.false;
     });
 
-    it('should have an elementwise equality check', function() {
-      v.elIsEqual([1, 4, 3]).isEqual([true, false, true]).should.be.true;
+    it('should have an elementwise equality check', () => {
+      v.elIsEqual([1, 4, 3]).isEqual([1, 0, 1]).should.be.true;
     });
 
-    it('should add two vectors', function() {
-      v = v.plus(w);
+    it('should have an addition method', () => {
+      x = v.plus(w);
+      x.isEqual([3, 5, 7]).should.be.true;
+      x.should.be.an.instanceof(Vector);
+    });
+
+    it('should have an addition assignment method', () => {
+      v.plusEq(w);
       v.isEqual([3, 5, 7]).should.be.true;
       v.should.be.an.instanceof(Vector);
     });
 
-    it('should subtract two vectors', function() {
-      v = v.minus(w);
+    it('should have a subtraction method', () => {
+      x = v.minus(w);
+      x.isEqual([-1, -1, -1]).should.be.true;
+      x.should.be.an.instanceof(Vector);
+    });
+
+    it('should have a subtraction assignment method', () => {
+      v.minusEq(w);
       v.isEqual([-1, -1, -1]).should.be.true;
       v.should.be.an.instanceof(Vector);
+    });
+
+    it('should have a scalar multiplication method', () => {
+      x = v.times(5);
+      x.isEqual([5, 10, 15]).should.be.true;
+      x.should.be.an.instanceof(Vector);
+    });
+
+    it('should have a scalar multiplication assignment method', () => {
+      v.timesEq(5);
+      v.isEqual([5, 10, 15]).should.be.true;
+      v.should.be.an.instanceof(Vector);
+    });
+
+    it('should have a scalar division method', () => {
+      x = y.dividedBy(5);
+      x.isEqual(v).should.be.true;
+      x.should.be.an.instanceof(Vector);
+    });
+
+    it('should have a scalar division assignment method', () => {
+      y.dividedByEq(5);
+      y.isEqual(v).should.be.true;
+      y.should.be.an.instanceof(Vector);
+    });
+
+    it('should have an elementwise multiplication method', () => {
+      x = v.ewTimes(w);
+      x.isEqual([2, 6, 12]).should.be.true;
+      x.should.be.an.instanceof(Vector);
+    });
+
+    it('should have an elementwise multiplication assignment method', () => {
+      v.ewTimesEq(w);
+      v.isEqual([2, 6, 12]).should.be.true;
+      v.should.be.an.instanceof(Vector);
+    });
+
+    it('should have an elementwise division method', () => {
+      x = v.ewDividedBy(v);
+      x.isEqual([1, 1, 1]).should.be.true;
+      x.should.be.an.instanceof(Vector);
+    });
+
+    it('should have an elementwise division assignment method', () => {
+      v.ewDividedByEq(v);
+      v.isEqual([1, 1, 1]).should.be.true;
+      v.should.be.an.instanceof(Vector);
+    });
+
+    it('should have a method for raising each element to a power', () => {
+      x = v.toThe(3);
+      x.isEqual([1, 8, 27]).should.be.true;
+      x.should.be.an.instanceof(Vector);
+    });
+
+    it('should have a method for elementwise power raising', () => {
+      x = v.ewToThe(w);
+      x.isEqual([1, 8, 81]).should.be.true;
+      x.should.be.an.instanceof(Vector);
+    });
+
+    it('should have a dot product method', () => {
+      x = v.dot(w);
+      x.should.eql(20);
+    });
+
+    it('should have a method to return the norm of a vector', () => {
+      x = Vector.of(3, 4);
+      x.norm().should.eql(5);
     });
   });
 });
